7b9d6d6235c480aa598b99ca9e5e5d09e5d29d15
Yassir Benjilali <116286449+Yassirbenj@users.noreply.github.com>
2023-09-24 13:58:25 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,9 @@ st.title("ChatGPT-like clone")
 
 openai.api_key = st.secrets["openai"]
 
+
 messages = [
-    {"role": "system", "content": "You are a customer receiving a call from a sales person. you are an owner of a small size hotel"},
+    {"role": "system", "content": "You are a customer receiving a call from a sales person. you are an owner of a small size hotel. you are skeptical of new technologies. You sell mainly by word of mouth. but you have noticed a drop in your activity recently. you are stressed because you have a lot to manage at the time of the call."},
 ]
 
 if "openai_model" not in st.session_state:
