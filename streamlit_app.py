04b4935da024d4843c19f5d28bfb1af7c434927f
Yassir Benjilali <116286449+Yassirbenj@users.noreply.github.com>
2023-09-24 14:02:48 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ openai.api_key = st.secrets["openai"]
 
 
 messages = [
-    {"role": "system", "content": "You are a customer receiving a call from a sales person. you are an owner of a small size hotel. you are skeptical of new technologies. You sell mainly by word of mouth. but you have noticed a drop in your activity recently. you are stressed because you have a lot to manage at the time of the call."},
+    {"role": "system", "content": "You are a customer receiving a call from a sales person. you are an owner of a small size hotel. you are skeptical of new technologies. You sell mainly by word of mouth. but you have noticed a drop in your activity recently. you are stressed because you have a lot to manage at the time of the call. you respond with brief answers"},
 ]
 
 if "openai_model" not in st.session_state:
